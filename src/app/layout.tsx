7f6867f6cad0e96d4a9da73c64ac555ebf244e8b
gpedrosad <gpedrosa@uade.edu.ar>
2025-11-27 16:48:05 -0300

    cambios para vercel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import Script from "next/script";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react"
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -124,6 +126,7 @@ export default function RootLayout({
         )}
 
         {/* Analytics de Vercel (no bloqueante) */}
+        <Analytics />
       </body>
     </html>
   );
